8e937f1142488061afa607fc79c3836253284247
unknown <danielhagantaginting@gmail.com>
2024-04-19 09:29:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch('https://randomuser.me/api/');
+      if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+      const data = await response.json();
+      setUserData(data.results[0]); // Hanya mengambil satu pengguna
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Fetch data saat komponen dimuat
+
+  const handleRefresh = () => {
+    fetchData(); // Panggil fungsi fetchData untuk mendapatkan data baru
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Data:</h1>
+      {userData && (
+        <div>
+          <img src={userData.picture.large} alt="User" />
+          <p>Name: {userData.name.first} {userData.name.last}</p>
+          <p>Email: {userData.email}</p>
+          <p>Phone: {userData.phone}</p>
+        </div>
+      )}
+      <button onClick={handleRefresh}>Refresh</button> {/* Tambahkan tombol refresh */}
     </div>
   );
-}
+};
 
 export default App;
